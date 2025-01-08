11164dc4a83b0b250aec24d437ffd01e29ce1b00
Poke13617 <cte-mason.curtis@sllboces.org>
2025-01-08 18:30:56 +0000

    .

---
+++ exampleJSVariables.js
@@ -0,0 +1,9 @@
+// Introduction to JavaScript Variables
+
+// 1. Using var (older way of declaring variables, avoid in modern code)
+
+var age = "16"; // This is a variable number, specifically an integer
+
+console.log("Using var: Theage is " + age)
+
+// Change the value of a variable declared with let/var
